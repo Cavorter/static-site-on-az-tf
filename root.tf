8c95e2e0d20245817a20b2964e6bb2588042a42d
Nathan Stohlmann <nathan@virtualmicropolis.com>
2019-07-13 11:15:43 -0500

    Added 'prod' variable to control which LE server is used by default to prevent running into api caps

--- root.tf
+++ root.tf
@@ -11,8 +11,7 @@ provider "azuread" {
 }
 
 provider "acme" {
-  server_url = "https://acme-v02.api.letsencrypt.org/directory"
-  #server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
+  server_url = local.le_server
   version    = "~> 1.3"
 }
 
--- vars.tf
+++ vars.tf
@@ -1,25 +1,31 @@
+variable "prod" {
+  type        = bool
+  default     = false
+  description = "If 'true' will use the Let's Encrypt production server. Otherwise it will use the staging server to help protect from running into api quotas."
+}
+
 variable "location" {
-  type        = "string"
+  type        = string
   description = "The Azure region all resources will be created in"
 }
 
 variable "domain" {
-  type        = "string"
+  type        = string
   description = "The domain to configure"
 }
 
 variable "pem_file_path" {
-  type        = "string"
+  type        = string
   description = "The path to the private key pem file used for CA registration"
 }
 
 variable "registration_address" {
-  type        = "string"
+  type        = string
   description = "The email address to register with the CA"
 }
 
 variable "vault_user" {
-  type        = "string"
+  type        = string
   description = "The object id of microsoft login for the account that will be white-listed in the key vault. You can use the Get-AzAdUser command to retrieve this field."
 }
 
@@ -36,4 +42,8 @@ variable "txt_records" {
 }
 
 locals {
+  le_server_prod  = "https://acme-v02.api.letsencrypt.org/directory"
+  le_server_stage = "https://acme-staging-v02.api.letsencrypt.org/directory"
+  le_server       = var.prod ? local.le_server_prod : local.le_server_stage
 }
+
