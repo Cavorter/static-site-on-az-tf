26b33e8249fef3ae541a96fb56e79720d6e83c55
Nathan Stohlmann <nathan@virtualmicropolis.com>
2020-08-15 18:32:33 -0500

    Making the dns module block optional based on the prod variable value

--- root.tf
+++ root.tf
@@ -34,6 +34,8 @@ module "ad" {
 }
 
 module "dns" {
+  count = var.prod ? 1 : 0
+  
   source               = "./modules/dns"
   domain               = var.domain
   location             = var.location
